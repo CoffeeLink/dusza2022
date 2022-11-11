2201df7cd47a4fffeb09f5e6ed211af55130b55b
Demeter Áron <demeter.aron0614@gmail.com>
2022-11-11 18:55:44 +0100

    Inital

--- config/config.php
+++ config/config.php
@@ -1,6 +1,8 @@
+<?php
 return array(
     'db_user'=> 'duszagfq_web_user',
     'db_password'=> 'Kortefa345',
     'db_name'=> 'duszagfq_blog_engine',
     'db_host'=> 'localhost:3306',
-)
+    'jwt_secret'=> 'AMOGUS',
+);
--- index.php
+++ index.php
@@ -5,15 +5,20 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Hi</title>
   <link rel="stylesheet" href="/css/main.css">
   <script defer src="/js/app.js"></script>
 </head>
 
 <body>
-  <?php
-  print "Hello world";
-    ?>
-</body>
+<?php
 
+if (!isset($_COOKIE['token'])) {
+  header('Location: /login.php');
+} else {
+  header('Location: /');
+}
+
+?>
+</body>
 </html>
