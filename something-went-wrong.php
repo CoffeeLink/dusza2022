40e2b6d017fd98efaae7d59db72cf0172ddbe63b
Zoli06 <zolixvagyok@gmail.com>
2022-11-12 22:58:40 +0100

    XSS fix

--- something-went-wrong.php
+++ something-went-wrong.php
@@ -3,7 +3,7 @@ $errorTitle = $_GET['errorTitle'] ?? 'Hiba történt';
 $errorDescription = $_GET['errorDescription'] ?? 'Hiba történt';
 $errorCode = $_GET['errorCode'] ?? '500';
 
-echo "<h1>$errorTitle</h1>";
-echo "<p>$errorDescription</p>";
-echo "<p>$errorCode</p>";
+echo "<h1>" . htmlspecialchars($errorTitle) . "</h1>";
+echo "<p>" . htmlspecialchars($errorDescription) . "</p>";
+echo "<p>" . htmlspecialchars($errorCode) . "</p>";
 ?>
