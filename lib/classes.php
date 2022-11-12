81555235a5d0677d37afb744b220475595691a28
Demeter Áron <demeter.aron0614@gmail.com>
2022-11-12 13:45:32 +0100

    Added Auth stuff

---
+++ lib/classes.php
@@ -0,0 +1,9 @@
+<?php
+
+// for user access control
+abstract class Permissions {
+    const Editor = "EDITOR";
+    const Moderator = "MODERATOR";
+    const Webmaster = "WEBMASTER";
+}
+
--- lib/connection.php
+++ lib/connection.php
@@ -1,15 +1,17 @@
 <?php
-
+//JWT token validation
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 require_once(__DIR__."/../vendor/autoload.php");
 
+//szimpla mysql kapcsolat létrehozása a config.php-ban megadott adatok alapján
 function connect_mysql() {
     $config = require(__DIR__."/../config/config.php");
     $db = new PDO('mysql:host=' . $config['db_host'] . ';dbname=' . $config['db_name'], $config['db_user'], $config['db_password']);
     return $db;
 }
 
+//megnézi, hogy a login évrényes-e, Ha igen, akkor visszaadja a felhasználó adatait
 function authorize($username, $password) {
     $db = connect_mysql();
     $query = $db->prepare('SELECT * FROM users WHERE user_name = :username AND password = :password');
@@ -22,6 +24,7 @@ function authorize($username, $password) {
     return $result;
 }
 
+//id alapu felhasználó lekérdezés
 function get_user_by_id($id) {
     $db = connect_mysql();
     $query = $db->prepare('SELECT * FROM users WHERE user_id = :id');
@@ -33,12 +36,34 @@ function get_user_by_id($id) {
     return $result;
 }
 
+//JWT token ellenőrzése, ha érvényes, akkor visszaadja a felhasználó ID-ját
 function validate_token($token) {
     $config = require(__DIR__."/../config/config.php");
     $decoded = JWT::decode($token, new Key($config['jwt_secret'], 'HS512'));
     $user = get_user_by_id($decoded->uid);
     if ($user) {
-        return true;
+        return $user['user_id'];
+    } else {
+        return false;
+    }
+}
+
+//Megnézi hogy a felhasználo nak van-e jogosultsága, ha igen, akkor True, ha nem, akkor False, JWT token alapján
+function checkPermission($token, $permission) {
+    $id = validate_token($token);
+    if ($id) {
+        $db = connect_mysql();
+        $query = $db->prepare('SELECT * FROM users WHERE user_id = :id');
+        $query->execute([
+            'id' => $id
+        ]);
+        $result = $query->fetch(PDO::FETCH_ASSOC);
+        $db = null;
+        if ($result['permission'] == $permission) {
+            return true;
+        } else {
+            return false;
+        }
     } else {
         return false;
     }
