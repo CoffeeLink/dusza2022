aced955cd4eb4f670dc3e2a2397420630de23605
Zoli06 <zolixvagyok@gmail.com>
2022-11-13 23:26:54 +0100

    Profile instead of search

--- modules/header.php
+++ modules/header.php
@@ -1,11 +1,27 @@
 <?php
 require_once __DIR__ . "/../lib/utils.php";
+require_once __DIR__ . "/../config/config.php";
+$base_url = (require __DIR__ . "/../config/config.php")['base_url'];
 // Start the session.
 if (!isset($_SESSION)) {
     session_start();
 }
 
 $token = $_SESSION['jwt_token'] ?? null;
+
+// Get user name if logged in.
+if (checkPermission($token, 'EDITOR')) {
+    $user_id = getUserId($token);
+
+
+    $pdo = connect_mysql();
+    $sql = "SELECT * FROM users WHERE user_id = ?";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$user_id]);
+    $user = $stmt->fetch();
+
+    $pdo = null;
+}
 
 $settings = json_decode(file_get_contents(__DIR__ . "/../settings/settings.json"), true);
 ?>
@@ -70,15 +86,27 @@ $settings = json_decode(file_get_contents(__DIR__ . "/../settings/settings.json"
                 <a href="./login.php" class="nav-link">Bejelentkezés</a>
                 <?php endif; ?>
                 </li>
-                <li class="nav-item search-item">
+                <!-- <li class="nav-item search-item">
                     <input type="search" class="form-control" placeholder="Keresés" aria-label="Search" />
                 </li>
 
-                <!-- Keresés -->
                 <li class="nav-item">
                     <button type="submit" class="btn-primary btn kereses">
                         <i class="fa fa-search"></i>
                     </button>
-                </li>
+                </li> -->
+
+                <?php if (isset($_SESSION['jwt_token'])): ?>
+                <a href="#" class="d-flex align-items-center link-dark text-decoration-none" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                <strong class="px-2">
+                    <?php
+                    echo htmlspecialchars($user['last_name']) . ' ' . htmlspecialchars($user['first_name']) . ' (' . htmlspecialchars($user['user_name']) . ')';
+                    ?>
+                </strong>
+                <img src="./img/default_profile_picture.png" alt="" width="32" height="32"
+                    class="rounded-circle me-2 profile_pic" />
+                <?php endif; ?>
+            </a>
             </ul>
         </header>
