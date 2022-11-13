c38586a6e6b08f2d01f973277343629ddf9a086a
Demeter Áron <demeter.aron0614@gmail.com>
2022-11-13 20:58:36 +0100

    Fixed Some user stuff

--- handlers/submit-delete-user.php
+++ handlers/submit-delete-user.php
@@ -4,12 +4,14 @@ require __DIR__ . "/../lib/utils.php";
 $config = require(__DIR__ . '/../config/config.php');
 $base_url = (require __DIR__ . "/../config/config.php")['base_url'];
 
+session_start();
+
 $token = $_SESSION['jwt_token'] ?? null;
 if (!checkPermission($token, 'WEBMASTER')) {
-    header("Location: $base_url/something-went-wrong.php?errorTitle=You%20do%20not%20have%20permission%20to%20edit%20users.&errorDescription=You%20do%20not%20have%20permission%20to%20edit%20users.&errorCode=403");
+    header("Location: $base_url/something-went-wrong.php?code=403");
     return;
 }
-$user_id = $_GET['user_id'];
+$user_id = $_GET['user'];
 
 $pdo = connect_mysql();
 $sql = "DELETE FROM users WHERE user_id = ?";
