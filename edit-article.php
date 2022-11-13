e49ca874d2db5598d0ab629be8c53b5bf18d9d9d
Ceglédi Zente Holló <nadizente@gmail.com>
2022-11-13 13:52:07 +0100

    Bejegyzés hozzáadása

--- edit-article.php
+++ edit-article.php
@@ -35,48 +35,46 @@ $page_title = "Szerkesztés";
 $pdo = null;
 include __DIR__ . "/header.php";
 ?>
+<div class="row">
+    <div class="col-xl-2 col-lg-1 col-sm-0"></div>
+    <div class="col-xl-8 col-lg-10 col-sm-12 border border-info rounded bg-info bg-opacity-10 px-4">
+        <form action="./handlers/submit-edit-article.php" method="POST">
+            <h1>Szerkesztés: <?php echo htmlspecialchars($title); ?></h1>
 
-<body>
-    <div class="row">
-        <div class="col-xl-2 col-lg-1 col-sm-0"></div>
-        <div class="col-xl-8 col-lg-10 col-sm-12 border border-info rounded bg-info bg-opacity-10 px-4">
-            <form action="./handlers/submit-edit-article.php" method="POST">
-                <h1>Szerkesztés: <?php echo htmlspecialchars($title); ?></h1>
-
-                <input type="hidden" name="article_id" value="<?php echo htmlspecialchars($article_id); ?>">
-                <div class="mb-3 row">
-                    <label for="title" class="col-12 col-sm-3 col-form-label">Cím:</label>
-                    <div class="col-12 col-sm-9">
-                        <input type="text" class="form-control" name="title" id="title" placeholder="Cím"
-                            value="<?php echo htmlspecialchars($title); ?>">
-                    </div>
+            <input type="hidden" name="article_id" value="<?php echo htmlspecialchars($article_id); ?>">
+            <div class="mb-3 row">
+                <label for="title" class="col-12 col-sm-3 col-form-label">Cím:</label>
+                <div class="col-12 col-sm-9">
+                    <input type="text" class="form-control" name="title" id="title" placeholder="Cím"
+                        value="<?php echo htmlspecialchars($title); ?>">
                 </div>
-                <div class="mb-3 row">
-                    <label for="description" class="col-12 col-sm-3 col-form-label">Leírás:</label>
-                    <div class="col-12 col-sm-9">
-                        <input type="text" class="form-control" name="description" id="description"
-                            placeholder="••••••••" value="<?php echo htmlspecialchars($description); ?>" required>
-                    </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="description" class="col-12 col-sm-3 col-form-label">Leírás:</label>
+                <div class="col-12 col-sm-9">
+                    <input type="text" class="form-control" name="description" id="description" placeholder="Leírás"
+                        value="<?php echo htmlspecialchars($description); ?>" required>
                 </div>
-                <div class="mb-3 row">
-                    <label for="description" class="col-12 col-sm-3 col-form-label">Tartalom:</label>
-                    <div class="col-12 col-sm-9">
+            </div>
+            <div class="mb-3 row">
+                <label for="description" class="col-12 col-sm-3 col-form-label">Tartalom:</label>
+                <div class="col-12 col-sm-9">
 
-                        <textarea class="form-control" name="content" id="content-editor" placeholder="Tartalom"
-                            id="floatingTextarea" rows="20"><?php echo htmlspecialchars($content); ?></textarea>
-                    </div>
-                    <input class="btn btn-success teljes my-4 py-2" type="submit" value="Mentés">
+                    <textarea class="form-control" name="content" id="content-editor" placeholder="Tartalom"
+                        id="floatingTextarea" rows="20"><?php echo htmlspecialchars($content); ?></textarea>
                 </div>
+                <input class="btn btn-success teljes my-4 py-2" type="submit" value="Mentés">
+            </div>
 
 
 
 
-            </form>
+        </form>
 
-        </div>
-        <div class="col-xl-2 col-lg-1 col-sm-0"></div>
     </div>
+    <div class="col-xl-2 col-lg-1 col-sm-0"></div>
+</div>
 
-    <?php
-  include __DIR__ . "./footer.php";
-  ?>
+<?php
+include __DIR__ . "./footer.php";
+?>
