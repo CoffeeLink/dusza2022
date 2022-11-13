5b59e8e4b7576f5dabd7d10e87ea551d19ec6f3a
Ceglédi Zente Holló <nadizente@gmail.com>
2022-11-13 11:46:47 +0100

    Header, footer, error oldal

---
+++ header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Bootstrap behívása -->
+    <link rel="stylesheet" href="./css/bootstrap/css/bootstrap.min.css" />
+    <script src="./css/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Font Awesome behívása -->
+    <script src="https://kit.fontawesome.com/df92ea1e57.js" crossorigin="anonymous"></script>
+
+    <!-- Saját CSS behívása -->
+    <link rel="stylesheet" href="./css/page.css" />
+    <title>Blogmotor</title>
+</head>
+
+<body>
+    <!-- Weboldalt körbefoglaló div -->
+    <div class="container">
+        <!-- Navigációs sor behívása -->
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+            <!-- Weboldal főcímének megjelenítése -->
+            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                <span class="fs-4">Weboldal címe</span>
+            </a>
+
+            <!-- Navigációs sor hivatkozásai -->
+            <ul class="nav nav-pills">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Főoldal</a>
+                </li>
+                <li class="nav-item"><a href="#" class="nav-link">Oldal1</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Oldal2</a></li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Bejelentkezés</a>
+                </li>
+                <li class="nav-item search-item">
+                    <input type="search" class="form-control" placeholder="Keresés" aria-label="Search" />
+                </li>
+
+                <!-- Keresés -->
+                <li class="nav-item">
+                    <button type="submit" class="btn-primary btn kereses">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </li>
+            </ul>
+        </header>
--- something-went-wrong.php
+++ something-went-wrong.php
@@ -1,9 +1,26 @@
 <?php
+
 $errorTitle = $_GET['errorTitle'] ?? 'Hiba történt';
 $errorDescription = $_GET['errorDescription'] ?? 'Hiba történt';
 $errorCode = $_GET['errorCode'] ?? '500';
-
-echo "<h1>" . htmlspecialchars($errorTitle) . "</h1>";
-echo "<p>" . htmlspecialchars($errorDescription) . "</p>";
-echo "<p>" . htmlspecialchars($errorCode) . "</p>";
+include __DIR__ . "/header.php";
 ?>
+<div class="row py-5 error_kulso">
+    <div class="col-sm-12 col-md-6 px-2">
+        <img class="error img-fluid img-thumbnail" src="./img/error.png" alt="Valami hiba történt">
+    </div>
+    <div class="col-sm-12 col-md-6 px-5 py-3">
+        <h2><?= $errorTitle ?></h2>
+
+        <div class="description py-2"><?= $errorDescription ?></div>
+        <div class="code py-2">Hibakód: <b><?= $errorCode ?></b></div>
+        <div class="py-3">A hiba okozta kellemetlenségért elnézést kérünk.</div>
+        <a href="./" class="btn btn-primary my-4 jobbra">Vissza a főoldalra</a>
+    </div>
+</div>
+
+
+</div>
+</body>
+
+</html>
