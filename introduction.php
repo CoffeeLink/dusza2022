28c10c98a36a21798d85085448008aa84fe106f2
Zoli06 <zolixvagyok@gmail.com>
2022-11-13 21:06:58 +0100

    Introducion page

--- introduction.php
+++ introduction.php
@@ -1,4 +1,7 @@
 <?php
+require __DIR__ . "/lib/utils.php";
+$settings = json_decode(file_get_contents(__DIR__ . "/settings/settings.json"), true);
+
 $page_title = "Bemutaatkozás";
 include __DIR__ . "/modules/header.php";
 ?>
@@ -8,14 +11,8 @@ include __DIR__ . "/modules/header.php";
 
     <div class="col-xl-8 col-lg-10 col-md-12">
         <h1>Bemutatkozás</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam officiis vel et provident hic rem, quas quia
-            minima doloribus temporibus. Labore ipsum necessitatibus laborum reprehenderit rem perspiciatis aut iusto
-            amet magnam quisquam ipsam autem temporibus in sint recusandae soluta, itaque tempora, maxime vero, voluptas
-            velit. Sit ipsum quas in. Veniam dolor esse voluptatem labore mollitia vel, praesentium doloribus itaque
-            atque ullam necessitatibus. Rem libero quia, sint ducimus officia beatae ea perspiciatis veritatis saepe
-            nesciunt eos esse enim vero consectetur iste vitae, voluptate debitis harum. Fugit quidem, totam quae
-            assumenda accusantium ipsum voluptatum minus, aliquid eligendi dolorem, perspiciatis omnis pariatur
-            repellendus!
+        <p>
+            <?= $settings['description'] ?>
         </p>
     </div>
 
